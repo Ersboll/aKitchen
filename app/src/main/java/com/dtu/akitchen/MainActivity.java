a91587f627f2614e146c2385bb04118ed1649828
Peetzie <s174159@student.dtu.dk>
2021-06-16 14:57:04 +0200

    Added submenus, logout button removed, changed selectedTab marked theme, new logout button

--- app/src/main/java/com/dtu/akitchen/MainActivity.java
+++ app/src/main/java/com/dtu/akitchen/MainActivity.java
@@ -1,17 +1,26 @@
 package com.dtu.akitchen;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import com.dtu.akitchen.authentication.logInOut;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.annotation.NonNull;
+import androidx.core.internal.view.SupportMenuItem;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 import com.dtu.akitchen.ui.main.SectionsPagerAdapter;
 import com.dtu.akitchen.databinding.ActivityMainBinding;
@@ -19,7 +28,7 @@ import com.dtu.akitchen.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private Button LogOutButton;
+    private String TAG = "ClickedLogout";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +37,20 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
+        
+
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = binding.viewPager;
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = binding.fab;
+        //Adding support for submenus
+        MaterialToolbar toolbar =  binding.topAppBar;
+        setSupportActionBar(toolbar);
 
-        LogOutButton = binding.button;
+
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,8 +60,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        LogOutButton.setOnClickListener(v -> {
-            logInOut.logout();
-        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mini_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.Logout_submenu:
+                Log.d(TAG,"Logout submenu clicked");
+                logInOut.logout();
+                return true;
+            case R.id.Delete_User_SubMenu:
+                Log.d(TAG, "Delete user submenu clicked");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
