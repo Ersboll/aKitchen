9e7a9103a42cdb040690bb833392d96cf80127fa
Ersboll <nker@dtu.dk>
2021-06-16 10:27:03 +0200

    Added a temporary logout button

--- app/src/main/java/com/dtu/akitchen/MainActivity.java
+++ app/src/main/java/com/dtu/akitchen/MainActivity.java
@@ -1,7 +1,9 @@
 package com.dtu.akitchen;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.dtu.akitchen.ui.login_logout.LogOutActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
@@ -12,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.dtu.akitchen.ui.main.SectionsPagerAdapter;
 import com.dtu.akitchen.databinding.ActivityMainBinding;
@@ -19,6 +22,7 @@ import com.dtu.akitchen.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private Button LogOutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +38,19 @@ public class MainActivity extends AppCompatActivity {
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = binding.fab;
 
+        LogOutButton = binding.button;
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
+        });
+
+        LogOutButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LogOutActivity.class);
+            startActivity(intent);
         });
     }
 }
