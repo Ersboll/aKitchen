5a9d46178ac197e88407e70765b827e1e065ed08
Jonas M Peltonen <s204442@student.dtu.dk>
2021-06-21 13:21:33 +0200

    OverviewManager now successfully gets all data needed for the overview current page

---
+++ app/src/main/java/com/dtu/akitchen/overview/History.java
@@ -0,0 +1,11 @@
+package com.dtu.akitchen.overview;
+
+public class History {
+
+    //object for holding history information from firebase.
+    //Meant to be used in OverviewManager and HistoryFragment
+
+    public History(){
+
+    }
+}
